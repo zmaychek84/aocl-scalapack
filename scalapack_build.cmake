f353a0aa468f6c3677168cc657a59731843bb34b
Rahul Raj <Rahul.Raj@amd.com>
2024-12-13 10:05:36 +0530

    AOCL-ScaLAPACK: Updated CMake Minimum Version to 3.26
    Signed-off-by: Rahul<Rahul.Raj@amd.com>
    Change-Id: Id96aada0417a903ab83ba266cd140354ce030137

--- scalapack_build.cmake
+++ scalapack_build.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.22)
+cmake_minimum_required(VERSION 3.26)
 ###################################################################
 # The values in this section must always be provided
 ###################################################################
